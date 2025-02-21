5714207a8457a1ceff1a0e05a683cc4e56deea93
jeseungwook <jsw992@gmail.com>
2025-02-21 16:54:43 +0900

    init

---
+++ src/main/java/com/geukrock/geukrockapiserver/GeukrockApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.geukrock.geukrockapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeukrockApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeukrockApiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/geukrock/geukrockapiserver/GeukrockApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geukrock.geukrockapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeukrockApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
